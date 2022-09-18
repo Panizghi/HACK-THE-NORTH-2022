06398ac3e2c531736385cb825d35c04506aab91f
The Floating String <laurencel2001@gmail.com>
2022-09-18 03:22:28 -0400

    Create simulator for SDK view

---
+++ src/test_simulate_sdk_view.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+import time
+import json
+
+async def connect_to_client():
+    async with websockets.connect("ws://localhost:8000/ws/sdk_controller_socket") as websocket:
+        while True:
+
+            client_data = {"movement": "ok"}
+            await websocket.send(json.dumps(client_data))
+            server_data = await websocket.recv()
+            print(server_data)
+
+asyncio.run(connect_to_client())
